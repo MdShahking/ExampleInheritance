a1f94d85e0c6354e3b5acd46aeb9791b9cf002b2
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-08 20:22:52 +0530

    this is commited

---
+++ ExampleInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        public string Name;
+        public void display()
+        {
+            Console.WriteLine("I am an aimal");
+        } 
+        class Dog : Animal
+        {
+            public void getname ()
+            {
+                Console.WriteLine("My name is " + Name);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Dog labrador = new Dog();
+                labrador.Name = "Rohu";
+                labrador.display();
+                labrador.getname();
+                Console.ReadLine();
+            }
+        }
+    }
+}
